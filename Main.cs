58b4e8d4a750e208b95b63cf89987c560cd0a13b
Herp Derpinstine <herpderpinstine@gmail.com>
2020-09-14 06:22:43 -0500

    first commit

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using MelonLoader;
+
+namespace TestPlugin
+{
+    public static class BuildInfo
+    {
+        public const string Name = "TestPlugin"; // Name of the Plugin.  (MUST BE SET)
+        public const string Description = "Plugin for Testing"; // Description for the Plugin.  (Set as null if none)
+        public const string Author = "TestAuthor"; // Author of the Plugin.  (MUST BE SET)
+        public const string Company = null; // Company that made the Plugin.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Plugin.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Plugin.  (Set as null if none)
+    }
+
+    public class TestPlugin : MelonPlugin
+    {
+        public override void OnPreInitialization() // Runs before Game Initialization.
+        {
+            MelonLogger.Msg("OnPreInitialization");
+        }
+
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+            MelonLogger.Msg("OnApplicationStart");
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+            MelonLogger.Msg("OnApplicationQuit");
+        }
+
+        public override void OnPreferencesApplied() // Runs when Melon Preferences get saved.
+        {
+            MelonLogger.Msg("OnPreferencesApplied");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+### GENERAL INFORMATION:
+
+- An Example of a Plugin for [MelonLoader](https://github.com/LavaGang/MelonLoader).
+
+---
+
+### LICENSING & CREDITS:
+
+TestPlugin is licensed under the Apache License, Version 2.0. See [LICENSE](https://github.com/LavaGang/TestPlugin/blob/master/LICENSE.md) for the full License.
